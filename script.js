33627cbc113663fd0cad35cddb4522521f99ef83
townsendda@sandhills.edu <108563194+TDavid99@users.noreply.github.com>
2022-08-01 00:06:49 -0400

    added var lower,upper,number,special

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
 // Assignment Code
+var lettersNumbers = {
+  lowercase: "a,b,c,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z",
+  uppercase: "A,B,C,D,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z",
+  numeric: "1,2,3,4,5,6,7,8,9,0",
+  specialCharacters: "`!@#$%^&*()_+?/.{}",
+}
+
 function generatePassword() {
   var passwordLength = window.prompt("choose a length of your password between 8 to 128 characters");
   passwordLength = parseInt(passwordLength);
